47a8a66c4275badbdf1b41b40b9ee3f7cc16e9b8
Dmitriy Lunev <dima.lunev14@gmail.com>
2024-04-16 10:58:38 +0300

    not needed typing.

--- typings/dev_utils/fastapi/verbose_http_exceptions/utils.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Any, TypeAlias
-
-Location: TypeAlias = str | None
-Attribute: TypeAlias = str | None
-
-def resolve_error_location_and_attr(error: dict[str, Any]) -> tuple[Location, Attribute]: ...
--- typings/dev_utils/sqlalchemy/filters/guards.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Any, TypeGuard
-
-from dev_utils.sqlalchemy.filters.converters import AnyLookupMapping, LookupMappingWithNested
-from dev_utils.sqlalchemy.filters.types import OperatorFilterDict
-
-def is_dict_simple_filter_dict(value: dict[Any, Any]) -> TypeGuard[OperatorFilterDict]: ...
-def has_nested_lookups(mapping: AnyLookupMapping) -> TypeGuard[LookupMappingWithNested]: ...
