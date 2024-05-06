6d826b7e6d20d5da7c9457ffc3304457be48b502
Dmitriy Lunev <dima.lunev14@gmail.com>
2024-05-06 10:01:44 +0300

    v1.8.2: add fastapi verbose http exception schema with pydantic.

--- dev_utils/verbose_http_exceptions/fastapi/__init__.py
+++ dev_utils/verbose_http_exceptions/fastapi/__init__.py
@@ -1,6 +1,7 @@
-"""FastAPI extention for verbose http exceptions usage."""
+"""FastAPI extension for verbose http exceptions usage."""
 
 from .handlers import apply_all_handlers as apply_all_handlers
 from .handlers import (
     apply_verbose_http_exception_handler as apply_verbose_http_exception_handler,
 )
+from .schemas import VerboseHTTPExceptionSchema as VerboseHTTPExceptionSchema
