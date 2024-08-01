ff4f298cd6222fe60b6b8e39ea00caf718fbfdd9
Dmitriy Lunev <dima.lunev14@gmail.com>
2024-08-01 15:28:22 +0300

    v7.0.0: remove all optional depends and code (refactor).

--- dev_utils/sqlalchemy/guards.py
+++
@@ -1,8 +0,0 @@
-from typing import Any, TypeGuard
-
-from sqlalchemy.orm.attributes import QueryableAttribute
-
-
-def is_queryable_attribute(value: Any) -> "TypeGuard[QueryableAttribute[Any]]":  # noqa: ANN401
-    """TypeGuard, that check if object is QueryableAttribute."""
-    return isinstance(value, QueryableAttribute)
